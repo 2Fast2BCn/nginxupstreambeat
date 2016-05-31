4e1a15861a9b1d88506e42b4bb8b7ed3d6340b4b
Kobe Nys <kobe@allnice.eu>
2016-05-31 21:42:49 +0200

    Initial version (not working)

--- collector/upstream.go
+++ collector/upstream.go
@@ -1,7 +1,6 @@
 package collector
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -36,84 +35,22 @@ func (c *UpstreamCollector) Collect(u url.URL) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("HTTP%s", res.Status)
 	}
 
-	// Nginx stub status sample:
-	// Active connections: 1
-	// server accepts handled requests
-	//  7 7 19
-	// Reading: 0 Writing: 1 Waiting: 0
-	var re *regexp.Regexp
-	scanner := bufio.NewScanner(res.Body)
-
-	// Parse active connections.
-	scanner.Scan()
-	re = regexp.MustCompile("Active connections: (\\d+)")
-	var active int
-	if matches := re.FindStringSubmatch(scanner.Text()); matches == nil {
-		logp.Warn("Fail to parse active connections from Nginx stub status")
-		active = -1
-	} else {
-		active, _ = strconv.Atoi(matches[1])
-	}
-
-	// Skip request status headers.
-	scanner.Scan()
-
-	// Parse request status.
-	scanner.Scan()
-	re = regexp.MustCompile("\\s(\\d+)\\s+(\\d+)\\s+(\\d+)")
-	var (
-		accepts  int
-		handled  int
-		dropped  int
-		requests int
-		current  int
-	)
-	if matches := re.FindStringSubmatch(scanner.Text()); matches == nil {
-		logp.Warn("Fail to parse request status from Nginx stub status")
-		accepts = -1
-		handled = -1
-		dropped = -1
-		requests = -1
-		current = -1
-	} else {
-		accepts, _ = strconv.Atoi(matches[1])
-		handled, _ = strconv.Atoi(matches[2])
-		requests, _ = strconv.Atoi(matches[3])
-
-		dropped = accepts - handled
-		current = requests - c.requests
-
-		c.requests = requests
-	}
-
-	// Parse connection status.
-	scanner.Scan()
-	re = regexp.MustCompile("Reading: (\\d+) Writing: (\\d+) Waiting: (\\d+)")
-	var (
-		reading int
-		writing int
-		waiting int
-	)
-	if matches := re.FindStringSubmatch(scanner.Text()); matches == nil {
-		logp.Warn("Fail to parse connection status from Nginx stub status")
-		reading = -1
-		writing = -1
-		waiting = -1
-	} else {
-		reading, _ = strconv.Atoi(matches[1])
-		writing, _ = strconv.Atoi(matches[2])
-		waiting, _ = strconv.Atoi(matches[3])
-	}
+	// Nginx upstream status sample:
+	// {"servers": {
+	//	"total": 4,
+	//	"generation": 1,
+	//	"server": [
+	//		{"index": 0, "upstream": "websphere", "name": "10.60.42.8:9080", "status": "up", "rise": 366, "fall": 0, "type": "http", "port": 0},
+	//		{"index": 1, "upstream": "websphere", "name": "10.60.42.8:9081", "status": "up", "rise": 2560, "fall": 0, "type": "http", "port": 0},
+	//		{"index": 2, "upstream": "websphere", "name": "10.60.42.9:9080", "status": "up", "rise": 1861, "fall": 0, "type": "http", "port": 0},
+	//		{"index": 3, "upstream": "websphere", "name": "10.60.42.9:9081", "status": "up", "rise": 764, "fall": 0, "type": "http", "port": 0}
+	//	]
+	// }}
+	//res.Body
 
 	return map[string]interface{}{
-		"active":   active,
-		"accepts":  accepts,
-		"handled":  handled,
-		"dropped":  dropped,
-		"requests": requests,
-		"current":  current,
-		"reading":  reading,
-		"writing":  writing,
-		"waiting":  waiting,
+		"total":   active,
+		"generation":  accepts,
+		"server":  
 	}, nil
 }
